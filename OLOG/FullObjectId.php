2e99e4b63d28d6a3aa1e68604390a0741a6dcdad
Oleg Loginov <olognv@gmail.com>
2016-06-02 13:26:48 +0300

    1

---
+++ OLOG/FullObjectId.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OLOG;
+
+class FullObjectId
+{
+    static public function getFullObjectId($obj)
+    {
+        if (!is_object($obj)) {
+            return 'not_object';
+        }
+        $obj_id_parts = array();
+        $obj_id_parts[] = get_class($obj);
+        if (method_exists($obj, 'getId')) {
+            $obj_id_parts[] = $obj->getId();
+        }
+        return implode('.', $obj_id_parts);
+    }
+
+}

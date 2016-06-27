af224e29139ac11373ba2a99acf65452d132d1ae
ologinov <olognv@gmail.com>
2016-06-27 19:28:26 +0300

    1

--- OLOG/ConfWrapper.php
+++ OLOG/ConfWrapper.php
@@ -13,8 +13,62 @@ class ConfWrapper
         \OLOG\Assert::assert(self::$config_arr);
  		return self::$config_arr;
  	}
- 
+
+    static public function getRequiredValue($path){
+        $config_arr = self::get();
+
+        return self::getRequiredSubvalue($config_arr, $path);
+    }
+
+    static public function getRequiredSubvalue($arr, $path){
+        Assert::assert(!empty($path));
+        Assert::assert(is_array($arr));
+
+        $value = $arr;
+
+        $parts = explode(".", $path);
+
+        foreach ($parts as $part) {
+            if (isset($value[$part])) {
+                $value = $value[$part];
+            } else {
+                throw new \Exception('missing config key');
+            }
+        }
+
+        return $value;
+    }
+
+    static public function getOptionalSubvalue($arr, $path, $default = ''){
+        if (empty($path)) {
+            return $default;
+        }
+
+        $value = $arr;
+
+        $parts = explode(".", $path);
+
+        foreach ($parts as $part) {
+            if (isset($value[$part])) {
+                $value = $value[$part];
+            } else {
+                // key doesn't exist, fail
+                return $default;
+            }
+        }
+
+        return $value;
+    }
+
+    static public function getOptionalValue($path, $default = ''){
+        $config_arr = self::get();
+
+        return self::getOptionalSubvalue($config_arr, $path, $default);
+    }
+
     /**
+     * @deprecated
+     * 
      * Get value an array by using "root.branch.leaf" notation
      *
      * @param string $path   Path to a specific option to extract
@@ -22,7 +76,6 @@ class ConfWrapper
      * @return mixed
      */
     static public function value($path, $default = ''){
-    	
     	if (empty($path)) {
     		return '';
     	}

cb79628b632339148aaab9dc2cdfc50ad2cf299d
mkozlov1 <650465@gmail.com>
2016-08-08 11:15:47 +0300

    1

--- OLOG/POSTFileAccess.php
+++ OLOG/POSTFileAccess.php
@@ -30,13 +30,13 @@ namespace OLOG;
 class POSTFileAccess
 {
     protected static $errorCodeMessages = array(
-        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-        3 => 'The uploaded file was only partially uploaded',
-        4 => 'No file was uploaded',
-        6 => 'Missing a temporary folder',
-        7 => 'Failed to write file to disk',
-        8 => 'A PHP extension stopped the file upload'
+        UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+        UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+        UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded',
+        UPLOAD_ERR_NO_FILE => 'No file was uploaded',
+        UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder',
+        UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk',
+        UPLOAD_ERR_EXTENSION => 'A PHP extension stopped the file upload'
     );
 
     protected $original_file_name;
@@ -45,15 +45,68 @@ class POSTFileAccess
     protected $upload_error_code;
     protected $file_size;
 
-    public function __construct($key)
+    /**
+     * @param $key
+     * @return POSTFileAccess
+     */
+    public static function factory($key)
+    {
+        \OLOG\Assert::assert(array_key_exists($key, $_FILES));
+        \OLOG\Assert::assert(!is_array($_FILES[$key]['name']), 'multi file upload');
+        $obj = self::createObjFromArray($_FILES[$key]);
+        return $obj;
+    }
+
+    /**
+     * @param $key
+     * @return array[POSTFileAccess]
+     */
+    public static function factoryArray($key)
     {
         \OLOG\Assert::assert(array_key_exists($key, $_FILES));
 
-        $this->setFileSize($_FILES[$key]['size']);
-        $this->setOriginalFileName($_FILES[$key]['name']);
-        $this->setMimeType($_FILES[$key]['type']);
-        $this->setTempFilepath($_FILES[$key]['tmp_name']);
-        $this->setUploadErrorCode($_FILES[$key]['error']);
+        $file_post_arr = $_FILES[$key];
+        $post_file_access_arr = [];
+        if (!is_array($file_post_arr['name'])) {
+            return [self::createObjFromArray($file_post_arr)];
+        }
+
+        $file_count = count($file_post_arr['name']);
+        $file_keys = array_keys($file_post_arr);
+
+        for ($i = 0; $i < $file_count; $i++) {
+            $file_arr = array();
+            foreach ($file_keys as $key) {
+                $file_arr[$key] = $file_post_arr[$key][$i];
+            }
+            $post_file_access_arr[] = self::createObjFromArray($file_arr);
+        }
+
+        return $post_file_access_arr;
+    }
+
+    protected static function createObjFromArray($array)
+    {
+        $obj = new self();
+        \OLOG\Assert::assert(array_key_exists('name', $array));
+        $obj->setOriginalFileName($array['name']);
+
+        \OLOG\Assert::assert(array_key_exists('size', $array));
+        $obj->setFileSize($array['size']);
+
+        \OLOG\Assert::assert(array_key_exists('type', $array));
+        $obj->setMimeType($array['type']);
+
+        \OLOG\Assert::assert(array_key_exists('tmp_name', $array));
+        $obj->setTempFilepath($array['tmp_name']);
+
+        \OLOG\Assert::assert(array_key_exists('error', $array));
+        $obj->setUploadErrorCode($array['error']);
+
+        \OLOG\Assert::assert($obj->isOk(), self::$errorCodeMessages[$obj->getUploadErrorCode()]);
+        \OLOG\Assert::assert($obj->isUploadedFile(), 'The uploaded file was not sent with a POST request');
+
+        return $obj;
     }
 
     public function getOriginalFileName()
@@ -111,18 +164,8 @@ class POSTFileAccess
         return strtolower(pathinfo($this->original_file_name, PATHINFO_EXTENSION));
     }
 
-    public function validate($validators_arr, &$error_message)
+    public function validate($validators_arr, &$error_message = null)
     {
-        if ($this->isOk() == false) {
-            $error_message = self::$errorCodeMessages[$this->getUploadErrorCode()];
-            return false;
-        }
-
-        if ($this->isUploadedFile() === false) {
-            $error_message = 'The uploaded file was not sent with a POST request';
-            return false;
-        }
-
         foreach ($validators_arr as $validator_obj) {
             /**
              * @var $validator_obj POSTFileValidatorInterface

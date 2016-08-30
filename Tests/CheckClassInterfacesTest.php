4341b3ec3815c83919a48ac4d979c8ee6d164a08
ologinov <olognv@gmail.com>
2016-08-30 12:17:25 +0300

    1

--- Tests/CheckClassInterfacesTest.php
+++ Tests/CheckClassInterfacesTest.php
@@ -6,10 +6,21 @@ class CheckClassInterfacesTest extends PHPUnit_Framework_TestCase
     public function testExceptionIfClassNotImplementsInterface()
     {
         \OLOG\CheckClassInterfaces::exceptionIfClassNotImplementsInterface(\Tests\TestObject::class, \Tests\TestInterface::class);
-        
+
         $this->expectException('Exception');
         $this->expectExceptionMessage('does not implement interface');
 
         \OLOG\CheckClassInterfaces::exceptionIfClassNotImplementsInterface(stdClass::class, '');
+    }
+
+    public function testClassImplementsInterface()
+    {
+        $this->assertTrue(
+            \OLOG\CheckClassInterfaces::classImplementsInterface(\Tests\TestObject::class, \Tests\TestInterface::class)
+        );
+
+        $this->assertFalse(
+            \OLOG\CheckClassInterfaces::classImplementsInterface(\Tests\TestObject::class, \Tests\TestInterface2::class)
+        );
     }
 }
---
+++ Tests/TestInterface2.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Tests;
+
+
+interface TestInterface2
+{
+    public function getId();
+}

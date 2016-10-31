6667be74dd19884a43ff30f90641c7bd15491fd1
Антон Пинегин <pinegin.anton@gmail.com>
2016-10-31 18:30:09 +0300

    fix

--- OLOG/Preloader.php
+++ OLOG/Preloader.php
@@ -4,11 +4,11 @@ namespace OLOG;
 
 class PreLoader
 {
-	static public function getPreLoaderJsHtml()
+	static public function getPreloaderJsHtml()
 	{
 		ob_start(); ?>
 		<script>
-			var PreLoader = {
+			var preloader = {
 				init: function () {
 					this.preloader = '\
 						<div id="preloader" style="z-index: 100000;position: fixed;top: 0;bottom: 0;left: 0;right: 0;display: none;background-color: rgba(255, 255, 255, 0.6);">\
@@ -35,7 +35,7 @@ class PreLoader
 					this.$preloader.show();
 				}
 			};
-			PreLoader.init();
+			preloader.init();
 		</script>
 		<?php
 		$html = ob_get_clean();
